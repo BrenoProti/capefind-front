3da0f5f8dc5ddf72e5d023f6000adfa476e70286
Antony Rafael <antony.costa@mercadolivre.com>
2023-06-08 12:34:04 -0300

    Add MIUI and material-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import Button from '@mui/material/Button';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+
 function App() {
 
   return (
     <>
       <h1>Hello World !</h1>
+      <Button variant="contained">Button</Button>
+      <AccessAlarmIcon />
     </>
   )
 }
