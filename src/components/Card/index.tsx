65aa568561f017aee3b3936014dfd3e5163815f4
Antony Rafael <antony.costa@mercadolivre.com>
2023-06-14 21:37:21 -0300

    card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './styles.module.scss';
 
 interface CardProps {
@@ -13,16 +14,41 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ cardItem }) => {
-  console.log(cardItem);
+  const navigate = useNavigate();
+
+  const getDateYear = (date: string) => {
+    const dateObj = new Date(date);
+    return dateObj.getFullYear();
+  };
+
+  const descriptionFiltered = cardItem.description.length > 350 ? `${cardItem.description.slice(0, 350)}...` : cardItem.description;
+
+  const handleCardClick = () => {
+    const articleLink = cardItem.url;
+    if (articleLink) navigate(`/article?url=${articleLink}&search=${cardItem.title}`);
+  };
+
   return (
-    <div className={styles.card}>
-      <div className={styles.cardInfo}>
-        <h3>{cardItem.id}</h3>
+    <div className={styles.card} onClick={handleCardClick}>
+      <div className={styles.cardHead}>
         <h3>{cardItem.title}</h3>
-        <p>{cardItem.authors.join(", ")}</p>
-        <p>{cardItem.tags.join(", ")}</p>
-        <p>{cardItem.date}</p>
-        <p>{cardItem.description}</p>
+        <span>{getDateYear(cardItem.date)}</span>
+      </div>
+      <div className={styles.cardInfo}>
+        <p>
+          {cardItem.authors.length > 1 ? "Autores" : "Autor"}:{" "}
+          <span>{cardItem.authors.join(", ")}</span>
+        </p>
+        <p>
+          Palavras chave: 
+            {cardItem.tags.map((tag) => (
+              <span className={styles.tag} key={tag}>{tag}</span>
+            ))}
+        </p>
+      </div>
+      <div className={styles.cardDescription}>
+        <p>Resumo</p>
+        <p className={styles.description}>{descriptionFiltered}</p>
       </div>
     </div>
   );
